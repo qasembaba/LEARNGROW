c33d2baab3c9938f7dc983235a284def7176dcc9
Qababa1000 <deltagare@LEXJONKAF07>
2020-09-11 12:23:26 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LEARNGROW
+{
+    class Program
+    { 
+        static void Main (string []arg)
+        {
+            byte number =2 ;
+            int count = 10;
+            float totalPrice = 20.95f;
+            char character = 'A';
+            string firstName = "Mosh";
+            bool isWorking = false; 
+
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
+
+        }
+    }
+}
